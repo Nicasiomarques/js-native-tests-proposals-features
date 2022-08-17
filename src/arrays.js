68e7ca3a0ee166083f48790eba4e842040f3c2ad
Nicasiomarques <nicasiomarques18@gmail.com>
2022-08-17 12:12:21 +0100

    test: should sort array mutable way

---
+++ src/arrays.js
@@ -0,0 +1,11 @@
+import 'core-js/actual/array/index.js';
+import { it, describe } from 'node:test';
+import assert from 'node:assert';
+
+describe('Old (mutable) array functions', () => {
+  const array = [1, 3, 2];
+  it('should sort the array', () => {
+    array.sort();
+    assert.deepStrictEqual(array, [1, 2, 3]);
+  })
+})
